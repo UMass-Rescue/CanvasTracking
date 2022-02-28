f145c67c60d090914fa7ed110e221fd987a56a55
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2022-02-28 12:20:00 -0500

    app is pushing data to the server

--- CanvasTrackingApp/App.js
+++ CanvasTrackingApp/App.js
@@ -4,7 +4,10 @@ import * as Location from "expo-location";
 import * as TaskManager from "expo-task-manager";
 import * as Permissions from "expo-permissions";
 import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+const BACKEND_SERVER = "http://localhost:8000/"
 const LOCATION_TRACKING = "location-tracking";
 
 export default function App() {
@@ -22,6 +25,12 @@ export default function App() {
     );
     console.log("tracking started?", hasStarted);
   };
+
+  const stopLocationTracking = async () => {
+    return await Location.hasStartedLocationUpdatesAsync(
+      LOCATION_TRACKING
+    );
+  }
 
   React.useEffect(() => {
     const config = async () => {
@@ -62,8 +71,21 @@ export default function App() {
       <Button
         title="Start Tracking"
         onPress={() => {
+          axios.post(BACKEND_SERVER + "register", {
+            name: text,
+          });
+          AsyncStorage.setItem('canvas_tracking_name', text);
           startLocationTracking();
           setStartedTracking(true);
+        }}
+      />
+      <Button
+        title="Stop Tracking"
+        onPress={async () => {
+          AsyncStorage.setItem('canvas_tracking_name', "");
+          let x = await stopLocationTracking();
+          console.log("tracking stopped?", x)
+          setStartedTracking(false);
         }}
       />
     </View>
@@ -89,11 +111,20 @@ TaskManager.defineTask(LOCATION_TRACKING, async ({ data, error }) => {
     const { locations } = data;
     let lat = locations[0].coords.latitude;
     let long = locations[0].coords.longitude;
+    let name = await AsyncStorage.getItem('canvas_tracking_name');
 
     // HERE is where we will put our server communication logic
 
-    // console.log(
-    //   `${new Date(Date.now()).toLocaleString()}: ${lat},${long}`
-    // );
+    if (name) {
+      axios.post(BACKEND_SERVER + "location", {
+        name: name,
+        latitude: lat,
+        longitude: long,
+      });
+      console.log(
+        `${new Date(Date.now()).toLocaleString()}: ${lat},${long},${name}`
+      );
+    }
+    
   }
 });
